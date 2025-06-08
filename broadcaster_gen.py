71c266298a72be836732726b9a18cc9d0cdfffb0
kaira <justaidajam@gmail.com>
2025-06-08 07:49:39 +0100

    #

--- broadcaster_gen.py
+++ broadcaster_gen.py
@@ -2,6 +2,7 @@ from database.kneo.ai_agent_generator import populate_ai_agents
 from database.kneo.brand_generator import generate_brands
 from database.kneo.genres_generator import generate_genres
 from database.kneo.listener_generator import generate_listeners
+from database.kneo.memory_generator import populate_brand_memories
 from database.kneo.music_label_generator import generate_labels
 from database.kneo.soundfragment_generator import generate_sound_fragments
 from database.kneo.profile_generator import generate_profiles
@@ -12,5 +13,6 @@ if __name__ == "__main__":
     generate_brands()
     generate_listeners()
     generate_profiles()
+    populate_brand_memories()
     generate_genres()
     generate_sound_fragments()
